4f94b3c67828f43635b0aa3059a4ef088589bb50
TerminatorOTW <devsiddhartha31@gmail.com>
2026-07-08 14:11:30 +0530

    fix #2: updated project structure to fix deployment issues

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "" : "",
 };
 
 export default nextConfig;
