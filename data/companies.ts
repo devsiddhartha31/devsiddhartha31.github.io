d310d751d1287b8bd122b5a55fa6a5920c36cfcd
TerminatorOTW <devsiddhartha31@gmail.com>
2026-07-10 11:04:46 +0530

    updated trusted companies layout and added items

---
+++ data/companies.ts
@@ -0,0 +1,30 @@
+import Company from "@/types/company";
+
+export const companies: Company[] = [
+  {
+    id: 1,
+    name: "Milo",
+    logo: "/images/companies/milo.png",
+    url: "",
+  },
+  {
+    id: 2,
+    name: "Kitkat",
+    logo: "/images/companies/kitkat.jfif",
+  },
+  {
+    id: 3,
+    name: "Sunsilk",
+    logo: "/images/companies/sunsilk.png",
+  },
+  {
+    id: 4,
+    name: "Kwality Walls",
+    logo: "/images/companies/kwality_walls.jfif",
+  },
+  {
+    id: 5,
+    name: "Hindustan Uniliever",
+    logo: "/images/companies/hul.jfif",
+  },
+];
