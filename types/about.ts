ae119e979c72b77949442c081d1c34fe79405de4
TerminatorOTW <devsiddhartha31@gmail.com>
2026-07-10 13:16:51 +0530

    fix #5: fixed AboutHighlights missing issue

--- types/about.ts
+++ types/about.ts
@@ -1,5 +1,26 @@
-export interface AboutItem {
+export type AboutIcon =
+  | "users"
+  | "building"
+  | "rocket"
+  | "brain"
+  | "experience";
+
+export type AboutCardSize =
+  | "normal"
+  | "span-c2-r1"
+  | "span-c1-r2";
+
+export interface AboutHighlight {
+  id: number;
+  value: string;
   title: string;
   subtitle: string;
-  highlights: string[];
+  icon: AboutIcon;
+  size: AboutCardSize;
+}
+
+export interface About {
+  title: string;
+  subtitle: string;
+  highlights: AboutHighlight[];
 }
