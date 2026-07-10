bb11fe26a1a3cf77b226125edd6e51a7993446fb
TerminatorOTW <devsiddhartha31@gmail.com>
2026-07-10 12:55:12 +0530

    updated hero background to be curved from the bottom and updated companies list with their respective logos

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -5,7 +5,7 @@ import GradientBackground from "@/components/ui/GradientBackground";
 
 export default function Hero() {
   return (
-    <section className="relative overflow-hidden">
+    <section className="relative overflow-hidden rounded-b-[48px] border-b border-white/5 shadow-[0_30px_80px_rgba(0,0,0,0.45)] bg-zinc-900/70 backdrop-blur-xl border border-white/10">
       <GradientBackground />
       <Container>
         <div className="flex min-h-[calc(100vh)] items-center py-22">
--- data/companies.ts
+++ data/companies.ts
@@ -27,4 +27,14 @@ export const companies: Company[] = [
     name: "Hindustan Uniliever",
     logo: "/images/companies/hul.jfif",
   },
+  {
+    id: 6,
+    name: "Moonglade Communication",
+    logo: "/images/companies/moonglade_communication.jpg",
+  },
+  {
+    id: 7,
+    name: "Digital Trubine",
+    logo: "/images/companies/digital_turbine.jpg",
+  },
 ];
